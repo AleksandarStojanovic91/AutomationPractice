7e01f902baaa8b07d7bdfedfdc84142d01719059
milenajanjic <mjanjic@peak-sourcing.com>
2020-07-31 20:50:17 +0200

    Nastavak forme

--- src/test/java/MilenaJanjic.java
+++ src/test/java/MilenaJanjic.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 import java.util.concurrent.TimeUnit;
 
@@ -31,7 +32,26 @@ public class MilenaJanjic {
             driver.findElement(By.cssSelector("#email_create")).sendKeys("asd123dsf@gmail.com");
             Thread.sleep(5000);
             driver.findElement(By.cssSelector(".btn.btn-default.button.button-medium.exclusive")).click();
+            driver.findElement(By.cssSelector("#SubmitCreate")).click();
             Thread.sleep(5000);
+            driver.findElement(By.cssSelector("#id_gender1")).click();
+            driver.findElement(By.cssSelector("#customer_firstname")).sendKeys("Milena");
+            driver.findElement(By.cssSelector("#customer_lastname")).sendKeys("Janjic");
+            driver.findElement(By.cssSelector("#email")).clear();
+            driver.findElement(By.cssSelector("#email")).sendKeys("asd123dsf@gmail.com");
+            driver.findElement(By.cssSelector("#passwd")).sendKeys("qqqqw");
+            Thread.sleep(5000);
+
+            Select daySelect = new Select(driver.findElement(By.cssSelector("days")));
+            daySelect.selectByValue("7");
+
+            Select monthSelect = new Select(driver.findElement(By.cssSelector("month")));
+            monthSelect.selectByValue("9");
+
+            Select yearSelect = new Select(driver.findElement(By.cssSelector("year")));
+            yearSelect.selectByValue("1992");
+
+            System.out.println("asd123dsf@gmail.com");
         }
 
         @After
